8ec611c9d3831a354d3bd39d9e3b55ac81fa0a37
ronilpatil <ronilpatil2000@gmail.com>
2024-10-17 20:50:53 +0530

    bug fixes & enhancements

--- service/app_with_diskcache.py
+++ service/app_with_diskcache.py
@@ -11,24 +11,29 @@ app = FastAPI()
 pathlib.Path("./cache").mkdir(parents=True, exist_ok=True)
 cache = dc.Cache("./cache")  # Specify your cache directory
 
+
 @app.get("/")
-def read_root() : 
+def read_root():
     return "Hello World!"
 
+
 @app.get("/name/{user_name}")
-def get_passkey(user_name:str) :
+def get_passkey(user_name: str):
+    # if user_name present in cache, return it else return None
     cache_data = cache.get(user_name)
-    if cache_data :
-        print('cache hit!') 
-        return json.loads(cache_data)        # convert JSON to python dict
-    else : 
-        print('cache miss!')
-        r = {"user": user_name.title(), "passkey" : random.randint(00000, 99999)}
-        cache.set(user_name, json.dumps(r))        # json.dumps - convert JSON HTTP response type into JSON string, making it suitable for redis db
+    if cache_data:
+        print("cache hit!")
+        return JSONResponse(cache_data)  # convert JSON to JSON HTTP responce type
+    else:
+        print("cache miss!")
+        r = {"user": user_name.title(), "passkey": random.randint(00000, 99999)}
+        cache.set(user_name, json.dumps(r))
         return JSONResponse(r)
 
-if __name__ == "__main__" : 
+
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
 # install "httpie" to access API through "http :8000/name/..." cmd.
